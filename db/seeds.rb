725efeb75d042cbae68f5759b2e99f4046c8dcbc
Ana Paula Vale <apvalefin@gmail.com>
2019-03-09 11:22:00 +0000

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 User.where(
-  first_name: 'Gustavo',
-  last_name: 'Cunha',
+  first_name: 'Ana',
+  last_name: 'Vale',
   email: 'email@example.com',
   default_locale: :en
 ).first_or_create(password: 'password2018')
